8cbbbe922df64c5bfe5aed129d12825bd5a5d557
Jack Hogan <jackhogan11@gmail.com>
2024-12-11 14:35:06 -0500

    Added webring, links kinda broken but will redesign soon

--- src/routes/Link.svelte
+++ src/routes/Link.svelte
@@ -1,35 +1,39 @@
 <script lang="ts">
-    import './styles.css';
-    import type LinkData from '$lib/Link';
+	import './styles.css';
+	import type LinkData from '$lib/Link';
 
-    export let link: LinkData;
+	export let link: LinkData;
 </script>
 
 <a href={link.destination} target="_blank">
-    <div class="card glassBackground">
-        <slot />
-        <h3>{link.name}</h3>
-    </div>
+	<div class="card glassBackground">
+		<slot />
+		<h3>{link.name}</h3>
+	</div>
 </a>
 
 <style>
-    div {
-        padding: 1em 2em 0 2em;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        width: 120px;
-        height: auto;
-    }
+	div {
+		padding: 1em 2em 0 2em;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		width: 120px;
+		height: auto;
+	}
 
-    a {
-        margin: 1em;
-    }
+	a {
+		margin: 1em;
+	}
 
-    @media (max-width: 1000px) {
-        div {
-            width: 50%;
-        }
-    }
+	@media (max-width: 1000px) {
+		div {
+			width: 50%;
+		}
+	}
+
+	h3 {
+		text-align: center;
+	}
 </style>
