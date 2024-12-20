19f3e8c4ba11c9c4231ea5c22a377819ba540691
Jack Hogan <jackhogan11@gmail.com>
2024-12-20 14:11:22 -0500

    Dot highlighting

--- src/lib/dotsFrag.glsl
+++ src/lib/dotsFrag.glsl
@@ -3,15 +3,25 @@ uniform vec2 u_mouse; // Mouse position (normalized)
 uniform float u_radius; // Mouse interaction radius
 uniform vec2 u_resolution;
 uniform int u_dark;
+uniform vec4 u_box; // The highlighted box
+uniform float u_boxBrightness;
 
 #include oklch
+
+float pointIsInBox(vec2 point, vec4 box) {
+    vec2 withinBounds = step(box.xy, point) * step(point, box.zw);
+    return withinBounds.x * withinBounds.y;
+}
 
 void main() {
     // Calculate distance from mouse
     float dist = distance(gl_FragCoord.xy, vec2(u_mouse.x, u_resolution.y - u_mouse.y));
 
     // Calculate brightness (closer = brighter)
-    float brightness = 1.0 - smoothstep(0.0, u_radius, dist);
+    vec4 b = u_box;
+    b.w = u_resolution.y - b.w;
+    b.y = u_resolution.y - b.y;
+    float brightness = max(1.0 - smoothstep(0.0, u_radius, dist), pointIsInBox(gl_FragCoord.xy, b) * u_boxBrightness * 0.75);
 
     // Set dot color using OKLCH-inspired values
     float l = brightness;
--- src/lib/dotsVert.glsl
+++ src/lib/dotsVert.glsl
@@ -3,9 +3,21 @@ attribute vec2 a_position; // Dot position (normalized)
 uniform vec2 u_resolution; // Canvas resolution
 uniform vec2 u_mouse;
 uniform float u_radius;
+uniform vec4 u_box;
+uniform float u_boxBrightness;
+
+float pointIsInBox(vec2 point, vec4 box) {
+    vec2 withinBounds = step(box.xy, point) * step(point, box.zw);
+    return withinBounds.x * withinBounds.y;
+}
+
 void main() {
     // Convert position to clip space [-1, 1]
     vec2 clipSpace = (a_position / u_resolution) * 2.0 - 1.0;
+    vec4 b = u_box;
+    b.y = u_resolution.y - b.y;
+    b.w = u_resolution.y - b.w;
+    vec4 boxClip = vec4(b.xy / u_resolution, b.zw / u_resolution) * 2.0 - 1.0;
     gl_Position = vec4(clipSpace * vec2(1, -1), 0, 1);
-    gl_PointSize = 4.0 + max(0.0, u_radius - distance(a_position, u_mouse)) / 25.0; // Base dot size
+    gl_PointSize = 4.0 + max(max(0.0, u_radius - distance(a_position, u_mouse)), pointIsInBox(clipSpace * vec2(1, -1), boxClip) * u_radius * u_boxBrightness * 0.75) / 25.0; // Base dot size
 }
