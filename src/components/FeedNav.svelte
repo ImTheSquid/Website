c6ba88c4ce2752fe58da01619cd5a2f8a7a1c7fa
Jack Hogan <jackhogan11@gmail.com>
2024-12-19 19:14:38 -0500

    Formatting improvements, started on Work section

--- src/components/FeedNav.svelte
+++ src/components/FeedNav.svelte
@@ -1,18 +1,27 @@
 <script lang="ts">
-  const { path }: {path: string} = $props();
+  const { path }: { path: string } = $props();
 
   interface Page {
     name: string;
     route: string;
   }
 
-  const pages: Page[] = [{name: "Me", route: "/"}, {name: "Work", route: "/work"}, {name: "Blog", route: "/blog"}];
+  const pages: Page[] = [
+    { name: "Me", route: "/" },
+    { name: "Work", route: "/work" },
+    { name: "Blog", route: "/blog" },
+  ];
 </script>
+
 <span class="flex flex-row text-2xl mb-5">
-{#each pages as {name, route}, i}
-    <a href={route} class="{path.match(`^${route}/?$`) ? 'font-bold' : 'underline'}">{name}</a>
+  {#each pages as { name, route }, i}
+    {#if !path.match(`^${route}/?$`)}
+      <a href={route} class="underline">{name}</a>
+    {:else}
+      <p class="font-bold">{name}</p>
+    {/if}
     {#if i < pages.length - 1}
-        <p class="mx-3 font-bold">/</p>
-        {/if}
-{/each}
+      <p class="mx-3 font-bold">/</p>
+    {/if}
+  {/each}
 </span>
