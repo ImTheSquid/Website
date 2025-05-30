da001be5237760ef834033b0d1b9dacd1b785a26
Jack Hogan <jackhogan11@gmail.com>
2025-05-30 17:29:19 -0400

    Enable vercel image service

--- astro.config.mjs
+++ astro.config.mjs
@@ -22,6 +22,8 @@ export default defineConfig({
     webAnalytics: {
       enabled: true,
     },
+    imageService: true,
+    devImageService: "sharp",
   }),
   site: "https://jackhogan.me",
   markdown: {
