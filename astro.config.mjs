c8af25f9a85a125d2138010d16a08ad86722333b
Jack Hogan <jackhogan11@gmail.com>
2026-02-06 16:08:03 -0500

    astro compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -16,6 +16,7 @@ import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 
 import icon from "astro-icon";
+import astroCompress from "gab-astro-compress";
 
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
@@ -23,7 +24,7 @@ import remarkGfm from "remark-gfm";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), svelte(), react(), icon()],
+  integrations: [mdx(), svelte(), react(), icon(), astroCompress()],
   adapter: vercel({
     webAnalytics: {
       enabled: true,
